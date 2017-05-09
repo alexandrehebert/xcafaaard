a4f36f4daf1075d08e315967eaaf9b550f57bb35
Alexandre HEBERT <ahebert@xebia.fr>
2017-05-09 15:54:10 +0200

    plus sandbox

---
+++ src/questions.js
@@ -0,0 +1,29 @@
+let express = require('express');
+let app = express();
+
+const answers = [];
+answers[/what is your name/g] = () => 'xcafard';
+
+app.get('/', (req, res) => {
+
+  const [id, q] = req.query.q.split(': ');
+
+  console.log(id, q);
+
+  answers.forEach((k, v) => {
+    console.log(k, v);
+  });
+
+  switch (q) {
+    case 'what is your name':
+      res.send('xcafaaard');
+      return;
+  }
+
+  res.send('');
+
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
