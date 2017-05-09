329b14ae0cfb6b7df85033a3659823d71b3c4d4e
Alexandre HEBERT <ahebert@xebia.fr>
2017-05-09 16:54:11 +0200

    fixed matching

--- src/question.js
+++ src/question.js
@@ -9,33 +9,34 @@ module.exports = query => {
 
   switch (true) {
 
-    case q.match('the largest'):
+    case !!q.match('the largest'):
       const i = q.match(numberPattern).map(parseInt);
       console.log(JSON.stringify(i));
       return Math.max(...i);
 
-    case q.match('what colour is a banana'):
+    case !!q.match('what colour is a banana'):
+      console.log('banana');
       return 'yellow';
 
-    case q.match('square and a cube'):
+    case !!q.match('square and a cube'):
       console.log('square');
       return '';
 
-    case q.match('what is your name'):
+    case !!q.match('what is your name'):
       return 'xcafaaard';
 
     case number.length > 0:
       console.log(JSON.stringify(number));
 
-        if (q.match('plus')) {
-            console.log(number[0] + number[1]);
-            return number[0] + number[1];
-        }
-        else if (q.match('multiplied by')) {
-            console.log(number[0] * number[1]);
-            return number[0] * number[1];
-        }
-        return;
+      if (q.match('plus')) {
+        console.log(number[0] + number[1]);
+        return number[0] + number[1];
+      }
+      else if (q.match('multiplied by')) {
+        console.log(number[0] * number[1]);
+        return number[0] * number[1];
+      }
+      return;
   }
 
   return '';
