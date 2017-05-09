22730e168c626b8d568550b3d462ca61bb449a6a
Alexandre HEBERT <ahebert@xebia.fr>
2017-05-09 16:35:48 +0200

    format

--- src/question.js
+++ src/question.js
@@ -4,24 +4,33 @@ module.exports = query => {
 
   console.log(query);
 
-  var numberPattern = /\d+/g;
-  var number = q.match(numberPattern) || [];
+  let numberPattern = /\d+/g;
+  let number = q.match(numberPattern) || [];
 
   switch (true) {
+
     case q.startsWith('which of the following numbers is the largest'):
       const i = n.split(', ').map(parseInt);
       console.log(JSON.stringify(i));
       return Math.max(i);
+
+    case q.startsWith('which of the following numbers is both a square and a cube'):
+      return '';
+
     case 'what is your name':
       return 'xcafaaard';
-    case number.length > 0: {
+
+    case number.length > 0:
       console.log(JSON.stringify(number));
-        if (q.find('plus')) {
-            return number[0] + number[1];
-        }
-        else if (q.find('moins')) {
-            return number[0] * number[1];
-        }
-    }
+      return number[0] + number[1];
+      if (q.find('plus')) {
+        return number[0] + number[1];
+      }
+      else if (q.find('moins')) {
+        return number[0] * number[1];
+      }
+
+    default:
+      return '';
   }
 };
